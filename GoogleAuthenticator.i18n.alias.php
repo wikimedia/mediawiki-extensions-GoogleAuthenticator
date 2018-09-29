7f199304ef34badc9e39e41f88779571eab6ceb4
Umherirrender <umherirrender_de.wp@web.de>
2018-09-29 11:57:56 +0200

    Add alias file for Special:Google2FARecover
    Change-Id: I8e8d54524ecab551aa2552505933bdcd5ac272d1

---
+++ GoogleAuthenticator.i18n.alias.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Aliases for special pages of the GoogleAuthenticator extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'Google2FARecover' => [ 'Google2FARecover' ],
+];
